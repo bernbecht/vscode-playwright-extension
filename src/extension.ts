ab7c8e30ac521bb007b6c56543833424b92aeb5a
Bernardo Bechtold <bernardobechtold@Bernardo-Bechtold-Mac-ASSET1464.local>
2024-10-01 11:03:53 -0300

    🛠️ update playwright execution script

--- src/extension.ts
+++ src/extension.ts
@@ -6,7 +6,7 @@ export function activate(context: vscode.ExtensionContext) {
         if (editor) {
             const filePath = editor.document.fileName;
             const terminal = vscode.window.createTerminal(`Playwright Test: ${filePath}`);
-            terminal.sendText(`npx playwright test ${filePath} --update-snapshots`);
+            terminal.sendText(`npm run test -- ${filePath} --update-snapshots`);
             terminal.show();
         }
     });
